ee8e57607cca3071b960783bf2e9f6e467d27cd9
devkledisom <dev@ziiz.com.br>
2025-12-19 08:43:18 -0300

    remover node modules1

--- src/utils/crypto.js
+++ src/utils/crypto.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
 const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN; // Ex: '1d', '8h'
